6cdb118089e39685a4b64b4bac2f13e1aed68977
Nilkanth <tatya922050@gmail.com>
2019-04-24 22:54:02 +0530

    Ionic app

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {InAppBrowser} from '@ionic-native/in-app-browser/ngx'
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+
+  constructor(private inBrowser: InAppBrowser){
+
+  }
+
+  opentab1(){
+    this.inBrowser.create("https://www.google.com",'_self');
+    console.log("hello");
+  }
 
 }
